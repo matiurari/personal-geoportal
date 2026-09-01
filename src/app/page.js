d6d10549b743fea2b1a7f11f9186b3ce29df4fda
matiurari <api.jakartasatu@gmail.com>
2026-09-01 18:39:33 +0700

    perubahan judul web

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div style={{ width: "100vw", height: "100vh", display: "flex" }}>
-      <h1>WebGIS</h1>
+    <div style={{ width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>Personal Geoportal</h1>
     </div>
   )
 }
