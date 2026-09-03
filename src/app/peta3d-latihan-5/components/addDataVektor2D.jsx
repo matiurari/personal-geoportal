2ab479fb22d05eadf55f15579fd69210ee7507bf
rezafahlevi <rezafahlevi.geodesi@gmail.com>
2026-09-03 23:52:51 +0700

    Peta 3D Latihan 5 - Visualisasi Data 2D dan 3D

---
+++ src/app/peta3d-latihan-5/components/addDataVektor2D.jsx
@@ -0,0 +1,44 @@
+export default function addDataVektor2D(viewer, opsi, selesai) {
+  const Cesium = window.Cesium;
+
+  const {
+    url,
+    nama = 'Data Vektor 2D',
+    warna = Cesium.Color.ORANGE,
+    warnaOutline = Cesium.Color.BLACK,
+    lebarGaris = 8,
+    nempelTerrain = true,
+    otomatisZoom = true,
+  } = opsi;
+
+  if (!url) {
+    if (selesai) selesai(null);
+    return;
+  }
+
+  Cesium.GeoJsonDataSource.load(url, {
+    clampToGround: nempelTerrain,
+  })
+    .then(function (dataSource) {
+      dataSource.name = nama;
+      viewer.dataSources.add(dataSource);
+
+      dataSource.entities.values.forEach(function (entity) {
+        if (entity.polyline) {
+          entity.polyline.material = new Cesium.PolylineOutlineMaterialProperty({
+            color: warna,
+            outlineWidth: 2,
+            outlineColor: warnaOutline,
+          });
+          entity.polyline.width = lebarGaris;
+          entity.polyline.clampToGround = nempelTerrain;
+        }
+      });
+
+      if (otomatisZoom) {
+        viewer.flyTo(dataSource);
+      }
+
+      if (selesai) selesai(dataSource);
+    })
+}
