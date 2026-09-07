122c77d6f22c08aa8a4abadc71a8c69b88079893
Ari_matiur <arimatiur0@gmail.com>
2026-09-07 09:22:35 +0700

    memberi title web

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "../theme/theme"
 import Providers from "./providers";
 
+export const metadata = {
+  title: "Geoportal",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
