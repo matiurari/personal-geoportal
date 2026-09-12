da802a660d37a770741c9074807726abbca5ec2d
matiurari <api.jakartasatu@gmail.com>
2026-09-12 23:55:47 +0700

    perbaikan pembentukan url getmodel di api create data3d

--- src/app/api/katalog-data-3d/create/route.js
+++ src/app/api/katalog-data-3d/create/route.js
@@ -51,7 +51,7 @@ export async function POST(request) {
         // Tulis file ke storage lokal
         await writeFile(filePath, buffer);
 
-        const fileUrl = `${process.env.NEXT_PUBLIC_URL_BASE_PATH}/api/katalog-data-3d/models/${data_3d_id}`;
+        const fileUrl = `${process.env.URL_BASE_PATH}/api/katalog-data-3d/models/${data_3d_id}`;
 
         // 3. Simpan ke Database Prisma dengan UUID yang sama
         await db.katalog_data_3d.create({
